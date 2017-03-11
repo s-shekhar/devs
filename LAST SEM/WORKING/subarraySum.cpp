4331092819498f9d29f8acec123755da47af0bbb
Shashank Shekhar <shashank24@outlook.com>
2017-03-11 17:20:08 +0530

    Find subarray with given sum(Nonnegative Numbers)

--- LAST SEM/WORKING/subarraySum.cpp
+++ LAST SEM/WORKING/subarraySum.cpp
@@ -7,17 +7,14 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {1, 4, 0, 0, 3, 10, 5}, sum = 7;
+    int arr[] = {1, 4, 0, 0, 3, 10, 5}, sum = 13;
     int limit = sizeof(arr)/sizeof(arr[0]);
-    int res = arr[0],total;
-
+    int total = arr[0],j=1;
 
     for(int i=0; i<limit-1; i++)
     {
-        total=arr[i];
-        int j = i+1;
 
-        while(total<sum)
+        while(total<sum && j<limit)
         {
             total += arr[j];
             j++;
@@ -32,7 +29,7 @@ int main()
         else
         {
             total -= arr[i]; //remove the first element since we cannot form a sub array with that first element since usko liya
-				//to exeed he hota hai sum. Ex 5,4,2 so sum 6 kay liye 5+4>6 so 5 toh aa he nhi sakta since 
+				//to exeed he hota hai sum. Ex 5,4,2 so sum 6 kay liye 5+4>6 so 5 toh aa he nhi sakta since
 				//5+4+2 will still be >6 so 5 hatana padega.
         }
 
